0494f7751d4534f121aa71f2d24e48e83db1c8b8
kuba <lnotspotl@gmail.com>
2024-10-18 16:12:15 -0400

    Add argparse as a dependency, link against yamlcpp statically

---
+++ ci/upload_zoo_helper.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -fL https://getcli.jfrog.io | sh
+
+export PATH_PREFIX=luxonis-zoo/depthai
+
+./jfrog config add --artifactory-url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
+./jfrog rt u "depthai_core/zoo_helper" "$PATH_PREFIX/"
