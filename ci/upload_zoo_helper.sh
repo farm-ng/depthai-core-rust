a26827895a4d019da152f320be37661780d072c0
lnotspotl <lnotspotl@gmail.com>
2024-10-18 18:30:51 -0400

    Update upload_zoo_helper.sh

--- ci/upload_zoo_helper.sh
+++ ci/upload_zoo_helper.sh
@@ -5,4 +5,4 @@ curl -fL https://getcli.jfrog.io | sh
 export PATH_PREFIX=luxonis-zoo/depthai
 
 ./jfrog config add --artifactory-url=$ARTIFACTORY_URL --user=$ARTIFACTORY_USER --password=$ARTIFACTORY_PASS
-./jfrog rt u "depthai_core/zoo_helper" "$PATH_PREFIX/"
+./jfrog rt u "build_core/zoo_helper" "$PATH_PREFIX/"
